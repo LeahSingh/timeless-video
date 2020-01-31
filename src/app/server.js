7685fe542e9692782389ea4810a6e634f1708c0b
Leah Singh <leahrsingh@gmail.com>
2020-01-31 02:15:41 -0600

    new edits

--- src/app/server.js
+++ src/app/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("./dist/tv"));
-app.get("/*", function(request, response) {
-  response.sendFile(path.join(__dirname, "/dist/atv/index.html"));
+app.use(express.static(__dirname + "/tv"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "tv", "index.html"));
 });
 app.listen(process.env.PORT || 8080);
